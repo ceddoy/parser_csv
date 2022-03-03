db66b8987af7af5ac86b137c2fee1809a41bdd82
andrew <ceddoy@mail.ru>
2022-03-04 05:07:31 +0700

    убрал items/

--- parser_csv/urls.py
+++ parser_csv/urls.py
@@ -3,12 +3,11 @@ from django.contrib import admin
 from django.urls import path
 
 from parser_csv import settings
-from parserapp.views import ItemCreateView, ItemListView
+from parserapp.views import ItemCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('upload_file/', ItemCreateView.as_view(), name='upload'),
-    path('items/', ItemListView.as_view(), name='items_list')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
