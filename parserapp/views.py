d93f278f63ae9abaaf35cc251b348441bdf27333
andrew <ceddoy@mail.ru>
2022-03-04 05:08:27 +0700

    после загрузки файла, вьюшка рендерит страницу с выгруженными данными

--- parserapp/views.py
+++ parserapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import HttpResponseRedirect
-from django.views.generic import CreateView, ListView
+from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.views.generic import CreateView
 
 from parserapp.forms import CSVFileForm
 from parserapp.models import Item
@@ -8,16 +9,9 @@ from parserapp.services import load_csv_file
 
 class ItemCreateView(CreateView):
     form_class = CSVFileForm
-    success_url = '/'
     template_name = 'parserapp/upload_csv_file.html'
 
     def form_valid(self, form):
         self.object = form.save()
-        Item.objects.bulk_create(load_csv_file(self.object.file.file.name))
-        return HttpResponseRedirect(self.get_success_url())
-
-
-class ItemListView(ListView):
-    model = Item
-    template_name = 'parserapp/items_list.html'
-    paginate_by = 100
+        new_data = Item.objects.bulk_create(load_csv_file(self.object.file.file.name))
+        return render(self.request, 'parserapp/items_list.html', {'object_list': new_data})
